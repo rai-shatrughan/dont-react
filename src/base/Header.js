effcf8ebb0670a739fd650fd399dd55c2905ea02
Shatrughan Rai <sr@sr.me>
2022-03-24 16:55:21 +0530

    css win

--- src/base/Header.js
+++ src/base/Header.js
@@ -1,34 +1,64 @@
+import React from "react";
 import 'purecss/build/buttons.css';
 import 'purecss/build/pure-min.css';
 import './Base.css';
+import logoDark from './assets/theme-dark.gif';
+import logoLight from './assets/theme-light.gif';
 
-const items = ["Home", "Stream", "ML", "Trends", "Metrics"];
+const items = ["Stream", "ML", "Trends", "Metrics"];
 
 function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
 
-function toggleTheme() {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-light');
-    } else {
-        setTheme('theme-dark');
+export default class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.toggleTheme = this.toggleTheme.bind(this);
+        this.state = {dark: false};
     }
-}
 
+    toggleTheme() {
+        if (localStorage.getItem('theme') === 'theme-dark') {
+            setTheme('theme-light');
+            this.setState({dark: false});
+        } else {
+            setTheme('theme-dark');
+            this.setState({dark: true});
+        }
+    }
 
-export default function Header() {
-  return (
-            <div className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
-                <ul className="pure-menu-list">
-                        {items.map((item) => 
-                            <li className="pure-menu-item" key={item}>
-                            <a href={item} className="pure-menu-link">{item}</a>
-                            </li>
-                        )}
-                    <li className="pure-menu-item switchTheme"><a href="#" onClick={toggleTheme} className="pure-menu-link">switchTheme</a></li>
-                </ul>                
-            </div>
-  );
+    componentDidMount() {
+        setTheme('theme-light');
+    }
+  
+  render(){
+    return (
+                <div className="pure-menu pure-menu-horizontal">
+                    <ul className="pure-menu-list">
+                        <li className="pure-menu-item">
+                            <a href="home" className="pure-menu-link">Home</a>
+                        </li>
+                        <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+                            <a href="#" className="pure-menu-link">Works</a>
+                            <ul className="pure-menu-children">
+                            {items.map((item) => 
+                                <li className="pure-menu-item" key={item}>
+                                    <a href={item} className="pure-menu-link">{item}</a>
+                                </li>
+                            )}    
+                            </ul>
+                        </li>
+                        <li className="pure-menu-item">
+                        { this.state.dark ?
+                            <img width="20" height="20" onClick={this.toggleTheme} src={logoDark} /> :
+                            <img width="20" height="20" onClick={this.toggleTheme} src={logoLight} /> 
+                        }
+                        </li>                    
+                    </ul>
+                </div>
+    );
+  }
 }
